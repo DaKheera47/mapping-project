445f8cdde7b2e1ea6464926d5f5c598706324942
DaKheera47 <shaheergoodboy@gmail.com>
2025-02-27 17:10:16 +0000

    robuster

--- src/components/SelectExample.tsx
+++ src/components/SelectExample.tsx
@@ -15,10 +15,17 @@ import { actions } from 'astro:actions';
 export default function SelectDemo() {
   const [value, setValue] = React.useState('');
   const [serverTime, setServerTime] = React.useState('');
+  const [error, setError] = React.useState('');
 
   return (
     <div className="mx-auto flex h-screen max-w-md flex-col items-center justify-center space-y-4">
-      <h1 className="text-4xl font-bold">{serverTime}</h1>
+      <div className="mb-8">
+        <h1 className="text-4xl font-bold">
+          {serverTime || 'No server time yet'}
+        </h1>
+
+        {error && <p className="text-red-500">{error}</p>}
+      </div>
 
       <Input
         placeholder="Enter your name"
@@ -27,11 +34,16 @@ export default function SelectDemo() {
       />
 
       <Button
-        onClick={() => {
-          actions.getServerTime({ name: value }).then(({ data }) => {
-            setServerTime(data ?? '');
-            setValue('');
-          });
+        onClick={async () => {
+          const { data, error } = await actions.getServerTime({ name: value });
+
+          if (error) {
+            setError(error.message);
+            return;
+          }
+
+          setServerTime(data ?? '');
+          setValue('');
         }}
       >
         Submit

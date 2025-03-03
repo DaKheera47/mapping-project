b76121702493c50869167f350e5daa7be31a4392
DaKheera47 <shaheergoodboy@gmail.com>
2025-03-03 23:20:34 +0000

    supabase, logins, sign in and register page, auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,20 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-
-import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react()],
-    vite: {
-        plugins: [tailwindcss()],
-    },
+	integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  output: 'server',
+	experimental: {
+		svg: {
+			mode: "sprite",
+		},
+	},
 });
